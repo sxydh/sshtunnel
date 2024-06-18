b211248d49373557c09189eb3088cc49418cbe28
sxydh <sxydhgg@gmail.com>
2024-06-18 12:56:48 +0800

    [UPDATE]

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using sshtunnel.Forms;
+using System;
 using System.Windows.Forms;
 
 namespace sshtunnel
@@ -16,7 +14,7 @@ namespace sshtunnel
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
