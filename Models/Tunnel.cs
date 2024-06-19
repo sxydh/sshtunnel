43dcba6fcb11e07effdd0c888fefa3e5d68fa2b3
sxydh <sxydhgg@gmail.com>
2024-06-19 19:26:43 +0800

    [UPDATE]

--- Models/Tunnel.cs
+++ Models/Tunnel.cs
@@ -9,25 +9,25 @@ namespace sshtunnel.Models
         public string SSHIp { get; set; }
 
         [JsonProperty("sshPort")]
-        public string SSHPort { get; set; }
+        public int SSHPort { get; set; }
 
         [JsonProperty("sshUser")]
         public string SSHUser { get; set; }
 
         [JsonProperty("listenPort")]
-        public string ListenPort { get; set; }
+        public int ListenPort { get; set; }
 
         [JsonProperty("targetIp")]
         public string TargetIp { get; set; }
 
         [JsonProperty("targetPort")]
-        public string TargetPort { get; set; }
+        public int TargetPort { get; set; }
 
         [JsonProperty("status")]
-        public string Status { get; set; }
+        public int Status { get; set; }
 
         [JsonProperty("delete")]
-        public string Delete { get; set; }
+        public int Delete { get; set; }
 
     }
 }
