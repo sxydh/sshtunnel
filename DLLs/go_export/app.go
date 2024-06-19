e715511f1f069395857ff764fe0c634a6353a016
sxydh <sxydhgg@gmail.com>
2024-06-20 02:49:40 +0800

    [UPDATE]

--- DLLs/go_export/app.go
+++ DLLs/go_export/app.go
@@ -30,7 +30,7 @@ func InitGoServer() int {
 	// 输出到控制台和文件
 	logPath := "./logs"
 	_ = os.Mkdir(logPath, os.ModePerm)
-	logPath += "/" + time.Now().Format("2006-01-02") + ".log"
+	logPath += "/" + time.Now().Format("2006-01-02") + ".go_export.log"
 	file, err := os.OpenFile(logPath, os.O_CREATE|os.O_APPEND|os.O_RDWR, os.ModePerm)
 	if err != nil {
 		fmt.Printf("OpenFile error: logPath=%v, err=%v\r\n", logPath, err)
