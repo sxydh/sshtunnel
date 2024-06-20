69d0da8eeb5f8c01869d683efea9d23f373ae2e9
sxydh <sxydhgg@gmail.com>
2024-06-20 12:34:56 +0800

    [UPDATE]

---
+++ DLLs/go_export/utils/common.go
@@ -0,0 +1,33 @@
+package utils
+
+import "io"
+
+type multiWriter struct {
+	writers []io.Writer
+}
+
+func (t *multiWriter) Write(p []byte) (n int, err error) {
+	for _, w := range t.writers {
+		n, err = w.Write(p)
+		if err != nil {
+			return
+		}
+		if n != len(p) {
+			err = io.ErrShortWrite
+			return
+		}
+	}
+	return len(p), nil
+}
+
+func MultiWriter(writers ...io.Writer) io.Writer {
+	allWriters := make([]io.Writer, 0, len(writers))
+	for _, w := range writers {
+		if mw, ok := w.(*multiWriter); ok {
+			allWriters = append(allWriters, mw.writers...)
+		} else {
+			allWriters = append(allWriters, w)
+		}
+	}
+	return &multiWriter{allWriters}
+}
