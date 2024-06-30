a23679be62129486b2a1d6cd27f9f0f0075a31cd
sxydh <sxydhgg@gmail.com>
2024-06-30 18:23:16 +0800

    [UPDATE]

---
+++ DLLs/ws_dll/app_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+func TestInitWsServer(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "Normal",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			InitWsServer()
+			select {}
+		})
+	}
+}
