0480cbda1f1f36d8211b167507a372f512e47a76
sxydh <sxydhgg@gmail.com>
2024-06-30 18:34:25 +0800

    [UPDATE]

--- DLLs/ws_dll/app_test.go
+++ DLLs/ws_dll/app_test.go
@@ -17,3 +17,19 @@ func TestInitWsServer(t *testing.T) {
 		})
 	}
 }
+
+func TestInitFsServer(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "Normal",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			InitFsServer()
+			select {}
+		})
+	}
+}
