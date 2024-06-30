bcd8dbf9d6f59c54591f0c04e767d1b26cd1caec
sxydh <sxydhgg@gmail.com>
2024-06-30 13:12:41 +0800

    [UPDATE]

---
+++ DLLs/go_export/app_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+func TestInitGoServer(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "Normal",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			InitGoServer()
+			select {}
+		})
+	}
+}
