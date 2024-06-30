a6c3ca163c4a79a0d6c9145b0448cdccd7c672e3
sxydh <sxydhgg@gmail.com>
2024-06-30 16:54:47 +0800

    [UPDATE]

--- VueUI/src/utils/json_util.ts
+++ VueUI/src/utils/json_util.ts
@@ -1,11 +1,10 @@
 export function stringify(p: any, exclusive?: string[]): string {
     let str = JSON.stringify(p)
-    if (exclusive) {
-        const json = JSON.parse(str)
-        for (let e of exclusive) {
-            delete json[e]
-        }
-        str = JSON.stringify(json)
+    exclusive = exclusive || ['_tmp']
+    const json = JSON.parse(str)
+    for (let e of exclusive) {
+        delete json[e]
     }
+    str = JSON.stringify(json)
     return str
 }
