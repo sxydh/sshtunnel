49225469b79a84271bea3a0fad4e48cd2d2f9e7f
sxydh <sxydhgg@gmail.com>
2024-07-03 21:28:12 +0800

    [INIT]

---
+++ poweredby-vue/Form1.Designer.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Web.WebView2.WinForms;
+using System.Windows.Forms;
+
+namespace poweredby_vue
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private WebView2 webView2;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            webView2 = new WebView2();
+            (webView2 as System.ComponentModel.ISupportInitialize).BeginInit();
+            webView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            webView2.Source = new System.Uri($"http://localhost:{fsPort}/?serverPort={wsPort}", System.UriKind.Absolute);
+            webView2.EnsureCoreWebView2Async().ContinueWith(p =>
+            {
+                Invoke((MethodInvoker)delegate
+                {
+                    webView2.CoreWebView2.Settings.AreDefaultContextMenusEnabled = false;
+                    webView2.CoreWebView2.Settings.IsWebMessageEnabled = true;
+                    webView2.CoreWebView2.WebMessageReceived += WebMessageReceived;
+                });
+            });
+            this.Controls.Add(webView2);
+            (webView2 as System.ComponentModel.ISupportInitialize).EndInit();
+
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1800, 900);
+            this.Text = "Form1";
+        }   
+
+    }
+}
+
---
+++ poweredby-vue/Form1.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace poweredby_vue
+{
+    public partial class Form1 : Form
+    {
+
+        private int fsPort;
+        private int wsPort;
+        private int closeFlag = -1;
+
+        public Form1()
+        {
+            BackColor = ColorTranslator.FromHtml("#212121");
+            StartPosition = FormStartPosition.CenterScreen;
+
+            fsPort = InitFsServer();
+            wsPort = InitWsServer();
+            InitializeComponent();
+
+            FormClosing += BeforeClose;
+        }
+
+        private void BeforeClose(object sender, FormClosingEventArgs e)
+        {
+            if (closeFlag <= 0)
+            {
+                closeFlag += 1;
+                e.Cancel = true;
+                webView2.ExecuteScriptAsync("window.beforeClose()");
+            }
+        }
+
+        private void WebMessageReceived(object sender, CoreWebView2WebMessageReceivedEventArgs e)
+        {
+            var msg = e.TryGetWebMessageAsString();
+            if (msg == "1")
+            {
+                closeFlag = 1;
+                Close();
+            }
+        }
+
+        [System.Runtime.InteropServices.DllImport("fs.dll", EntryPoint = "InitFsServer")]
+        private static extern int InitFsServer();
+
+        [System.Runtime.InteropServices.DllImport("ws.dll", EntryPoint = "InitWsServer")]
+        private static extern int InitWsServer();
+
+    }
+}
