2f55ac194f8052bd875bff569b499d5d99db9ffa
sxydh <sxydhgg@gmail.com>
2024-07-03 22:32:08 +0800

    [UPDATE]

--- poweredby-winform/Forms/MainForm.cs
+++ poweredby-winform/Forms/MainForm.cs
@@ -45,7 +45,7 @@ namespace poweredby_winform.Forms
             fileHelper.W(JsonConvert.SerializeObject(tl));
         }
 
-        [DllImport("tcp_server.dll")]
+        [DllImport("ts_dll.dll")]
         public static extern int InitGoServer();
     }
 }
